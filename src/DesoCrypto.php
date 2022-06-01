7e4ec0f0318000efd5360c66295c8f9ace0c0141
Null <this.git@azate.email>
2022-06-01 19:01:30 +0300

    add helpers for jwt and public key

--- src/DesoCrypto.php
+++ src/DesoCrypto.php
@@ -3,10 +3,62 @@
 namespace DesoSmart\DesoClient;
 
 use Elliptic\EC;
+use Exception;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 use Muvon\KISS\Base58Codec;
 
 final class DesoCrypto
 {
+    /**
+     * @throws DesoCryptoException
+     */
+    public static function removeNetworkPrefix(string $publicKey): string
+    {
+        return substr(self::publicKeyDecode($publicKey), 6);
+    }
+
+    /**
+     * @throws DesoCryptoException
+     */
+    public static function publicKeyDecode(string $publicKey): string
+    {
+        $hex = Base58Codec::checkDecode($publicKey);
+        if (!$hex) {
+            throw new DesoCryptoException('Invalid public key');
+        }
+
+        return $hex;
+    }
+
+    public static function hasValidJwt(string $publicKey, string $jwt): bool
+    {
+        try {
+            self::validateJwt($publicKey, $jwt);
+            return true;
+        } catch (Exception $_) {
+            return false;
+        }
+    }
+
+    /**
+     * @throws DesoCryptoException
+     */
+    public static function validateJwt(string $publicKey, string $jwt): void
+    {
+        $pkHex = self::publicKeyDecode($publicKey);
+
+        try {
+            $ec = new EC('secp256k1');
+            $pkFull = $ec->keyFromPublic(substr($pkHex, 6), 'hex')->getPublic(false, 'hex');
+            $pkBase64 = base64_encode(hex2bin('3056301006072a8648ce3d020106052b8104000a034200'.$pkFull));
+            $keyMaterial = sprintf('-----BEGIN PUBLIC KEY-----%s-----END PUBLIC KEY-----', PHP_EOL.$pkBase64.PHP_EOL);
+            JWT::decode($jwt, new Key($keyMaterial, 'ES256'));
+        } catch (Exception $e) {
+            throw new DesoCryptoException('Invalid verify public key', 0, $e);
+        }
+    }
+
     public static function prepareDerivedKeyForExtraData(string $derivedPublicKey): string
     {
         // Truncate network prefix
